74041cd20911e693b1b0a2a9997b45497428aecc
Nicolas Widart <n.widart@gmail.com>
2014-10-26 17:47:17 +0100

    Removing facade usage

--- Http/Filters/AdminFilter.php
+++ Http/Filters/AdminFilter.php
@@ -1,9 +1,9 @@
 <?php namespace Modules\Core\Http\Filters;
 
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\Session\Store;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Foundation\Application;
 use Modules\Core\Contracts\Authentication;
 
 class AdminFilter
@@ -16,11 +16,26 @@ class AdminFilter
      * @var SessionManager
      */
     private $session;
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var Redirector
+     */
+    private $redirect;
+    /**
+     * @var Application
+     */
+    private $application;
 
-    public function __construct(Authentication $auth, Store $session)
+    public function __construct(Authentication $auth, Store $session, Request $request, Redirector $redirect, Application $application)
     {
         $this->auth = $auth;
         $this->session = $session;
+        $this->request = $request;
+        $this->redirect = $redirect;
+        $this->application = $application;
     }
 
     public function filter()
@@ -28,17 +43,17 @@ class AdminFilter
         // Check if the user is logged in
         if (!$this->auth->check()) {
             // Store the current uri in the session
-            $this->session->put('url.intended', Request::url());
+            $this->session->put('url.intended', $this->request->url());
 
             // Redirect to the login page
-            return Redirect::route('login');
+            return $this->redirect->route('login');
         }
 
         // Check if the user has access to the dashboard page
         if ( ! $this->auth->hasAccess('dashboard.index'))
         {
             // Show the insufficient permissions page
-            return App::abort(403);
+            return $this->application->abort(403);
         }
     }
 }
