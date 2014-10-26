d6ca016246bde304bede384145b1b52b8de1ac35
Nicolas Widart <n.widart@gmail.com>
2014-10-26 17:40:43 +0100

    Using the authenticator contract

--- Http/Filters/AdminFilter.php
+++ Http/Filters/AdminFilter.php
@@ -1,17 +1,27 @@
 <?php namespace Modules\Core\Http\Filters;
 
-use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Session;
+use Modules\Core\Contracts\Authentication;
 
 class AdminFilter
 {
+    /**
+     * @var Authentication
+     */
+    private $auth;
+
+    public function __construct(Authentication $auth)
+    {
+        $this->auth = $auth;
+    }
+
     public function filter()
     {
         // Check if the user is logged in
-        if (!Sentinel::check()) {
+        if (!$this->auth->check()) {
             // Store the current uri in the session
             Session::put('url.intended', Request::url());
 
@@ -20,7 +30,7 @@ class AdminFilter
         }
 
         // Check if the user has access to the dashboard page
-        if ( ! Sentinel::getUser()->hasAccess('dashboard.index'))
+        if ( ! $this->auth->hasAccess('dashboard.index'))
         {
             // Show the insufficient permissions page
             return App::abort(403);
