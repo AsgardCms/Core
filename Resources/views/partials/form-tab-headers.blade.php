c201dc98151982d657cf639544586f653008f9a7
Nicolas Widart <n.widart@gmail.com>
2014-11-19 20:50:27 +0100

    Add the ul element

--- Resources/views/partials/form-tab-headers.blade.php
+++ Resources/views/partials/form-tab-headers.blade.php
@@ -1,3 +1,4 @@
+<ul class="nav nav-tabs">
 <?php $i = 0; ?>
 <?php foreach(LaravelLocalization::getSupportedLocales() as $locale => $language): ?>
     <?php $i++; ?>
@@ -5,3 +6,4 @@
         <a href="#tab_{{ $i }}" data-toggle="tab">{{ trans('core::core.tab.'. strtolower($language['name'])) }}</a>
     </li>
 <?php endforeach; ?>
+</ul>
