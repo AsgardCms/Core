f1ac540699c1d14e8765988e62f73658f4f8f1eb
Nicolas Widart <n.widart@gmail.com>
2015-02-11 20:52:10 +0100

    Translate in the partial

--- Resources/views/fields/select-theme.blade.php
+++ Resources/views/fields/select-theme.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label for="{{ $settingName }}">{{ $moduleInfo['description'] }}</label>
+    <label for="{{ $settingName }}">{{ trans($moduleInfo['description']) }}</label>
     <select class="form-control" name="{{ $settingName }}" id="{{ $settingName }}">
         <?php foreach ($themes as $name => $theme): ?>
             <option value="{{ $name }}" {{ isset($dbSettings[$settingName]) && $dbSettings[$settingName]->plainValue == $name ? 'selected' : '' }}>
