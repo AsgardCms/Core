cb534c92b0449e312c20a85faa5e96f22ad2cfc6
Nicolas Widart <n.widart@gmail.com>
2015-02-12 20:27:48 +0100

    Remove unneeded construct declaration

--- Console/PublishThemeAssetsCommand.php
+++ Console/PublishThemeAssetsCommand.php
@@ -7,11 +7,6 @@ class PublishThemeAssetsCommand extends Command
     protected $name = 'asgard:publish:theme';
     protected $description = 'Publish theme assets';
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function fire()
     {
         $this->call('stylist:publish');
