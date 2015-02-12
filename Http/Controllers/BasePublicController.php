839fa9e22d22316ede58f06a67dfec0c7a11b26e
Nicolas Widart <n.widart@gmail.com>
2015-02-12 15:35:11 +0100

    Move the register path for stylist

--- Http/Controllers/BasePublicController.php
+++ Http/Controllers/BasePublicController.php
@@ -1,23 +1,10 @@
 <?php namespace Modules\Core\Http\Controllers;
 
-use FloatingPoint\Stylist\Facades\StylistFacade as Stylist;
 use Illuminate\Routing\Controller;
-use Modules\Core\Contracts\Setting;
 
 abstract class BasePublicController extends Controller
 {
-    /**
-     * @var string The active theme name
-     */
-    public $theme;
-    /**
-     * @var Setting
-     */
-    private $setting;
-
     public function __construct()
     {
-        $this->setting = app('setting.settings');
-        Stylist::registerPath(base_path('Themes/' . $this->setting->get('core::template')), true);
     }
 }
