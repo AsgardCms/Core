be7626cb2e7838fd5a29fed581b28358e8f14a62
Nicolas Widart <n.widart@gmail.com>
2015-02-13 13:49:01 +0100

    Use the new Admin theme for administration

--- composers.php
+++ composers.php
@@ -1,6 +1,6 @@
 <?php
 
-View::creator('core::partials.sidebar-nav', 'Modules\Core\Composers\SidebarViewCreator');
-View::composer('core::layouts.master', 'Modules\Core\Composers\MasterViewComposer');
+View::creator('partials.sidebar-nav', 'Modules\Core\Composers\SidebarViewCreator');
+View::composer('layouts.master', 'Modules\Core\Composers\MasterViewComposer');
 View::composer('core::fields.select-theme', 'Modules\Core\Composers\ThemeComposer');
 View::composer('*', 'Modules\Core\Composers\LocaleComposer');
