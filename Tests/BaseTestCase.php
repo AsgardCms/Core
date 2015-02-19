1001caced168fc57a2b41a0223f93c6cc2c57e84
Nicolas Widart <n.widart@gmail.com>
2015-02-19 14:01:23 +0100

    Add the application trait

--- Tests/BaseTestCase.php
+++ Tests/BaseTestCase.php
@@ -1,14 +1,18 @@
 <?php namespace Modules\Core\Tests;
 
+use Illuminate\Foundation\Testing\ApplicationTrait;
 use TestCase;
 
 abstract class BaseTestCase extends TestCase
 {
+    use ApplicationTrait;
+
     protected $app;
 
     public function setUp()
     {
         parent::setUp();
+        $this->refreshApplication();
     }
 
     protected function checkResponseIsOkAndContains($request, $filter)
