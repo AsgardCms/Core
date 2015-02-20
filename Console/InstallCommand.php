d0638da27507ec6ca58b781887c518354e0478b9
Nicolas Widart <n.widart@gmail.com>
2015-02-20 12:05:14 +0100

    Add the welcome message as block

--- Console/InstallCommand.php
+++ Console/InstallCommand.php
@@ -50,7 +50,7 @@ class InstallCommand extends Command
      */
     public function fire()
     {
-        $this->info('Starting the installation process...');
+        $this->blockMessage('Welcome,', 'Starting the installation process...');
 
         $success = $this->installer->stack([
             'Modules\Core\Console\Installers\Scripts\ProtectInstaller',
