51fc553bdb94dfa242f7b555458f455c3707536c
Nicolas Widart <n.widart@gmail.com>
2015-02-26 11:49:25 +0100

    Add keypress as a default asset

--- Config/core.php
+++ Config/core.php
@@ -77,6 +77,7 @@ return [
            'jquery.dataTables.js',
            'dataTables.bootstrap.js',
            'jquery.slug.js',
+           'keypressAction.js',
            'app.js',
        ],
    ],
