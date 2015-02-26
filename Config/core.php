75a6d98c599ea2b718ecc018baa9df09bf3b7c27
Nicolas Widart <n.widart@gmail.com>
2015-02-26 10:25:19 +0100

    Add some default asset paths

--- Config/core.php
+++ Config/core.php
@@ -21,4 +21,34 @@ return [
     |--------------------------------------------------------------------------
     */
     'admin-theme' => 'AdminLTE',
+
+   /*
+   |--------------------------------------------------------------------------
+   | Define which assets will be available through the asset pipeline
+   |--------------------------------------------------------------------------
+   | These assets are registered on the asset manager
+   */
+    'admin-assets' => [
+        // Css
+        'bootstrap.css' => Theme::url('css/vendor/bootstrap.min.css'),
+        'font-awesome.css' => Theme::url('css/vendor/font-awesome.min.css'),
+        'ionicons.css' => Theme::url('css/vendor/ionicons.min.css'),
+        'alertify.core.css' => Theme::url('css/vendor/alertify/alertify.core.css'),
+        'alertify.default.css' => Theme::url('css/vendor/alertify/alertify.default.css'),
+        'dataTables.bootstrap.css' => Theme::url('css/vendor/datatables/dataTables.bootstrap.css'),
+        'icheck.blue.css' => Theme::url('css/vendor/iCheck/flat/blue.css'),
+        'AdminLTE.css' => Theme::url('css/AdminLTE.css'),
+        // Javascript
+        'jquery.js' => Theme::url('js/vendor/jquery.min.js'),
+        'bootstrap.js' => Theme::url('js/vendor/bootstrap.min.js'),
+        'mousetrap.js' => Theme::url('js/vendor/mousetrap.min.js'),
+        'alertify.js' => Theme::url('js/vendor/alertify/alertify.js'),
+        'icheck.js' => Theme::url('js/vendor/iCheck/icheck.min.js'),
+        'jquery.dataTables.js' => Theme::url('js/vendor/datatables/jquery.dataTables.js'),
+        'dataTables.bootstrap.js' => Theme::url('js/vendor/datatables/dataTables.bootstrap.js'),
+        'jquery.slug.js' => Theme::url('js/vendor/jquery.slug.js'),
+        'app.js' => Theme::url('js/app.js'),
+        'skeypressAction.js' => Module::asset('core:js/keypressAction.js'),
+        'ckeditor.js' => Theme::script('js/vendor/ckeditor/ckeditor.js'),
+    ]
 ];
