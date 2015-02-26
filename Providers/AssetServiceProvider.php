bb51dfb7b1d5d91ffaad7661374de60da1fa6a0f
Nicolas Widart <n.widart@gmail.com>
2015-02-26 09:45:26 +0100

    Move the asset bindings to dedicated service provider

---
+++ Providers/AssetServiceProvider.php
@@ -0,0 +1,31 @@
+<?php namespace Modules\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Core\Foundation\Asset\Manager\AsgardAssetManager;
+use Modules\Core\Foundation\Asset\Pipeline\AsgardAssetPipeline;
+
+class AssetServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     * @return void
+     */
+    public function register()
+    {
+        $this->bindAssetClasses();
+    }
+
+    /**
+     * Bind classes related to assets
+     */
+    private function bindAssetClasses()
+    {
+        $this->app->singleton('Modules\Core\Foundation\Asset\Manager\AssetManager', function () {
+            return new AsgardAssetManager();
+        });
+
+        $this->app->bind('Modules\Core\Foundation\Asset\Pipeline\AssetPipeline', function ($app) {
+            return new AsgardAssetPipeline($app['Modules\Core\Foundation\Asset\Manager\AssetManager']);
+        });
+    }
+}
