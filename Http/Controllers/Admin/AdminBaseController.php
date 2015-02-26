06fb58eb8215d8462797fa8d03981b45617613a0
Nicolas Widart <n.widart@gmail.com>
2015-02-26 10:25:49 +0100

    Add the assets from config on the asset manager

--- Http/Controllers/Admin/AdminBaseController.php
+++ Http/Controllers/Admin/AdminBaseController.php
@@ -1,11 +1,38 @@
 <?php namespace Modules\Core\Http\Controllers\Admin;
 
 use Illuminate\Routing\Controller;
+use Modules\Core\Foundation\Asset\Manager\AssetManager;
+use Modules\Core\Foundation\Asset\Pipeline\AssetPipeline;
 
 class AdminBaseController extends Controller
 {
+    /**
+     * @var AssetManager
+     */
+    private $assetManager;
+    /**
+     * @var AssetPipeline
+     */
+    private $assetPipeline;
+    /**
+     * @var \FloatingPoint\Stylist\Html\ThemeHtmlBuilder
+     */
+    private $theme;
 
     public function __construct()
     {
+        $this->assetManager = app('Modules\Core\Foundation\Asset\Manager\AssetManager');
+        $this->assetPipeline = app('Modules\Core\Foundation\Asset\Pipeline\AssetPipeline');
+        $this->theme = app('FloatingPoint\Stylist\Html\ThemeHtmlBuilder');
+
+        $this->addAssets();
+    }
+
+    /**
+     * Add the assets from the config file on the asset manager
+     */
+    private function addAssets()
+    {
+        $this->assetManager->addAssets(config('asgard.core.core.admin-assets'));
     }
 }
