8dcdc4f812c93ea8664b6c2d0f78a83e1e04256e
Nicolas Widart <n.widart@gmail.com>
2015-04-11 13:29:41 +0200

    Initialize selectize.js

--- Resources/views/fields/select-locales.blade.php
+++ Resources/views/fields/select-locales.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label for="{{ $settingName }}">{{ trans($moduleInfo['description']) }}</label>
-    <select multiple class="form-control" name="{{ $settingName }}[]" id="{{ $settingName }}">
+    <select multiple class="locales" name="{{ $settingName }}[]" id="{{ $settingName }}">
         <?php foreach ($locales as $id => $locale): ?>
         <option value="{{ $id }}" {{ isset($dbSettings[$settingName]) && isset(array_flip(json_decode($dbSettings[$settingName]->plainValue))[$id]) ? 'selected' : '' }}>
             {{ array_get($locale, 'name') }}
@@ -8,3 +8,11 @@
         <?php endforeach; ?>
     </select>
 </div>
+<script>
+    $( document ).ready(function() {
+        $('.locales').selectize({
+            delimiter: ',',
+            plugins: ['remove_button']
+        });
+    });
+</script>
