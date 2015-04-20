8ec6d09632f82e99820401691b5e1c38c13926e8
Nicolas Widart <n.widart@gmail.com>
2015-04-20 10:50:01 +0200

    Add textarea basic macro

--- macros.php
+++ macros.php
@@ -5,11 +5,21 @@
 | Translatable fields
 |--------------------------------------------------------------------------
 */
-Form::macro('i18nInput', function($name, $title, $errors, $lang)
-{
-    $string = "<div class='form-group " . ($errors->has($lang.'.'.$name) ? ' has-error' : '') . "'>";
+Form::macro('i18nInput', function ($name, $title, $errors, $lang) {
+    $string = "<div class='form-group " . ($errors->has($lang . '.' . $name) ? ' has-error' : '') . "'>";
     $string .= Form::label("{$lang}[{$name}]", $title);
-    $string .= Form::text("{$lang}[{$name}]", Input::old("{$lang}[title]"), ['class' => "form-control", 'placeholder' => $title]);
+    $string .= Form::text("{$lang}[{$name}]", Input::old("{$lang}[title]"),
+        ['class' => "form-control", 'placeholder' => $title]);
+    $string .= $errors->first("{$lang}.{$name}", '<span class="help-block">:message</span>');
+    $string .= "</div>";
+
+    return $string;
+});
+
+Form::macro('i18nTextarea', function ($name, $title, $errors, $lang) {
+    $string = "<div class='form-group " . ($errors->has($lang . '.' . $name) ? ' has-error' : '') . "'>";
+    $string .= Form::label("{$lang}[{$name}]", $title);
+    $string .= "<textarea class='ckeditor' name='{$lang}[body]' rows='10' cols='80'></textarea>";
     $string .= $errors->first("{$lang}.{$name}", '<span class="help-block">:message</span>');
     $string .= "</div>";
 
