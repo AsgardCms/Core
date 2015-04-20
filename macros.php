5fa079320a311bcd12e4d95bc3bc11ef3ee23567
Nicolas Widart <n.widart@gmail.com>
2015-04-20 11:35:13 +0200

    Fix name

--- macros.php
+++ macros.php
@@ -8,7 +8,7 @@
 Form::macro('i18nInput', function ($name, $title, $errors, $lang) {
     $string = "<div class='form-group " . ($errors->has($lang . '.' . $name) ? ' has-error' : '') . "'>";
     $string .= Form::label("{$lang}[{$name}]", $title);
-    $string .= Form::text("{$lang}[{$name}]", Input::old("{$lang}[title]"),
+    $string .= Form::text("{$lang}[{$name}]", Input::old("{$lang}[{$name}]"),
         ['class' => "form-control", 'placeholder' => $title]);
     $string .= $errors->first("{$lang}.{$name}", '<span class="help-block">:message</span>');
     $string .= "</div>";
@@ -19,7 +19,8 @@ Form::macro('i18nInput', function ($name, $title, $errors, $lang) {
 Form::macro('i18nTextarea', function ($name, $title, $errors, $lang) {
     $string = "<div class='form-group " . ($errors->has($lang . '.' . $name) ? ' has-error' : '') . "'>";
     $string .= Form::label("{$lang}[{$name}]", $title);
-    $string .= "<textarea class='ckeditor' name='{$lang}[body]' rows='10' cols='80'></textarea>";
+    $oldInput = Input::old("{$lang}[{$name}]");
+    $string .= "<textarea class='ckeditor' name='{$lang}[$name]' rows='10' cols='80'>{$oldInput}</textarea>";
     $string .= $errors->first("{$lang}.{$name}", '<span class="help-block">:message</span>');
     $string .= "</div>";
 
@@ -34,7 +35,7 @@ Form::macro('i18nTextarea', function ($name, $title, $errors, $lang) {
 Form::macro('normalInput', function ($name, $title, $errors) {
     $string = "<div class='form-group " . ($errors->has($name) ? ' has-error' : '') . "'>";
     $string .= Form::label($name, $title);
-    $string .= Form::text($name, Input::old("title"),
+    $string .= Form::text($name, Input::old($name),
         ['class' => "form-control", 'placeholder' => $title]);
     $string .= $errors->first($name, '<span class="help-block">:message</span>');
     $string .= "</div>";
