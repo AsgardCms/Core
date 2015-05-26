f882e4d0fda29f731e73891e61a0aa2a930f6a5f
Nicolas Widart <n.widart@gmail.com>
2015-05-26 16:42:47 +0200

    Add helper to get or set the current locale of the application

--- helpers.php
+++ helpers.php
@@ -6,3 +6,14 @@ if (! function_exists('on_route')) {
         return Route::current()->getName() == $route;
     }
 }
+
+if (! function_exists('locale')) {
+    function locale($locale = null)
+    {
+        if (is_null($locale)) {
+            return LaravelLocalization::getCurrentLocale();
+        }
+
+        return LaravelLocalization::setLocale($locale);
+    }
+}
