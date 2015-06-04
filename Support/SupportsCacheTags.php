4c97b9e8f576e1dfd349ec3c0efa5ea57a67a435
Nicolas Widart <n.widart@gmail.com>
2015-06-04 14:59:47 +0200

    Add a supports cache tags trait

---
+++ Support/SupportsCacheTags.php
@@ -0,0 +1,13 @@
+<?php namespace Modules\Core\Support;
+
+trait SupportsCacheTags
+{
+    /**
+     * Checks if the cache tags functionality is supported with the current driver
+     * @return bool
+     */
+    public function cacheTagsAreSupported()
+    {
+        return !in_array(config('cache.default'), ['file', 'database']);
+    }
+}
