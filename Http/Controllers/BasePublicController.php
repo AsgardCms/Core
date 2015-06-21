bc02012f6faaac4a177265928a9b09572da87ccd
Nicolas Widart <n.widart@gmail.com>
2015-06-21 10:56:15 +0200

    Adding a currentUser variable to every public view

--- Http/Controllers/BasePublicController.php
+++ Http/Controllers/BasePublicController.php
@@ -4,7 +4,14 @@ use Illuminate\Routing\Controller;
 
 abstract class BasePublicController extends Controller
 {
+    /**
+     * @var \Modules\Core\Contracts\Authentication
+     */
+    private $auth;
+
     public function __construct()
     {
+        $this->auth = app('Modules\Core\Contracts\Authentication');
+        view()->share('currentUser', $this->auth->check());
     }
 }
