067c0639609794d48ad40df5afe8ff87b06aeb74
Nicolas Widart <n.widart@gmail.com>
2015-06-22 17:03:11 +0200

    Use the view() helper

--- composers.php
+++ composers.php
@@ -1,8 +1,8 @@
 <?php
 
-View::creator('partials.sidebar-nav', 'Modules\Core\Composers\SidebarViewCreator');
+view()->creator('partials.sidebar-nav', 'Modules\Core\Composers\SidebarViewCreator');
 view()->composer('partials.footer', \Modules\Core\Composers\ApplicationVersionViewComposer::class);
-View::composer('layouts.master', 'Modules\Core\Composers\MasterViewComposer');
-View::composer('core::fields.select-theme', 'Modules\Core\Composers\ThemeComposer');
-View::composer('core::fields.select-locales', 'Modules\Core\Composers\SettingLocalesComposer');
-View::composer('*', 'Modules\Core\Composers\LocaleComposer');
+view()->composer('layouts.master', 'Modules\Core\Composers\MasterViewComposer');
+view()->composer('core::fields.select-theme', 'Modules\Core\Composers\ThemeComposer');
+view()->composer('core::fields.select-locales', 'Modules\Core\Composers\SettingLocalesComposer');
+view()->composer('*', 'Modules\Core\Composers\LocaleComposer');
