c85c300b7dbf606a5c2585e74c3e3f3c99c4ee14
Nicolas Widart <n.widart@gmail.com>
2015-06-22 08:44:46 +0200

    Removing old sidebar view creator

--- composers.php
+++ composers.php
@@ -1,6 +1,5 @@
 <?php
 
-View::creator('partials.sidebar-nav', 'Modules\Core\Composers\SidebarViewCreator');
 View::composer('layouts.master', 'Modules\Core\Composers\MasterViewComposer');
 View::composer('core::fields.select-theme', 'Modules\Core\Composers\ThemeComposer');
 View::composer('core::fields.select-locales', 'Modules\Core\Composers\SettingLocalesComposer');
