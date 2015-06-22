1c04cb1ff72b6cc4ec0f742400124ac90b8ab8df
Nicolas Widart <n.widart@gmail.com>
2015-06-22 09:03:54 +0200

    Put the sidebar view creator back

--- composers.php
+++ composers.php
@@ -1,5 +1,6 @@
 <?php
 
+View::creator('partials.sidebar-nav', 'Modules\Core\Composers\SidebarViewCreator');
 View::composer('layouts.master', 'Modules\Core\Composers\MasterViewComposer');
 View::composer('core::fields.select-theme', 'Modules\Core\Composers\ThemeComposer');
 View::composer('core::fields.select-locales', 'Modules\Core\Composers\SettingLocalesComposer');
