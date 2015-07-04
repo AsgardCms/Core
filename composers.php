206f91a1dac1606395772011ae83f6e5edb3b262
Nicolas Widart <n.widart@gmail.com>
2015-07-04 15:56:19 +0200

    Use the ::class notation

--- composers.php
+++ composers.php
@@ -1,8 +1,8 @@
 <?php
 
-view()->creator('partials.sidebar-nav', 'Modules\Core\Composers\SidebarViewCreator');
+view()->creator('partials.sidebar-nav', \Modules\Core\Composers\SidebarViewCreator::class);
 view()->composer('partials.footer', \Modules\Core\Composers\ApplicationVersionViewComposer::class);
-view()->composer('layouts.master', 'Modules\Core\Composers\MasterViewComposer');
-view()->composer('core::fields.select-theme', 'Modules\Core\Composers\ThemeComposer');
-view()->composer('core::fields.select-locales', 'Modules\Core\Composers\SettingLocalesComposer');
-view()->composer('*', 'Modules\Core\Composers\LocaleComposer');
+view()->composer('layouts.master', \Modules\Core\Composers\MasterViewComposer::class);
+view()->composer('core::fields.select-theme', \Modules\Core\Composers\ThemeComposer::class);
+view()->composer('core::fields.select-locales', \Modules\Core\Composers\SettingLocalesComposer::class);
+view()->composer('*', \Modules\Core\Composers\LocaleComposer::class);
