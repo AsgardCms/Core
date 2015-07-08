04c58e205ac4508b1437fc3a06bed78faa6113c2
franciscpd <franciscpd@gmail.com>
2015-07-07 21:58:09 -0400

    Adding portuguese brasilian locale

---
+++ Resources/lang/pt-BR/core.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    'modal' => [
+        'title' => 'Confirmação',
+        'confirmation-message' => 'Você deseja excluir este registro?',
+    ],
+    'table' => [
+        'created at' => 'Criado em',
+        'actions' => 'Ações',
+        'thumbnail' => 'Miniatura',
+    ],
+    'tab' => [
+        'english' => 'Inglês',
+        'french' => 'Francês',
+        'dutch' =>  'Holandês',
+        'italian' => 'Italiano',
+        'greek' => 'Grego',
+    ],
+    'button' => [
+        'cancel' => 'Cancelar',
+        'create' => 'Adicionar',
+        'update' => 'Atualizar',
+        'delete' => 'Excluir',
+        'reset' => 'Reiniciar',
+        'update and back' => 'Atualizar e voltar',
+    ],
+    'breadcrumb' => [
+        'home' => 'Home',
+    ],
+    'title' => [
+        'translatable fields' => 'Campos a traduzir',
+        'non translatable fields' => 'Campos não traduzidos',
+        'create resource' => 'Adicionar :name',
+        'edit resource' => 'Editar :name',
+    ],
+    'general' => [
+        'available keyboard shortcuts' => 'Teclas de atalho disponíveis nesta página'
+    ],
+    'messages' => [
+        'resource created' => ':name criado com sucesso.',
+        'resource not found' => ':name não encontrado.',
+        'resource updated' => ':name atualizado com sucesso.',
+        'resource deleted' => ':name excluido com sucesso.',
+    ],
+    'back' => 'Back',
+    'back to index' => 'Voltar para :name',
+    'permission denied' => 'Permissão negada. (Permissão requerida: ":permission")',
+];
