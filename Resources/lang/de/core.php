06cb7614ee6a085b3451578a904c5f2d29629f12
Nuno Ribeiro <nmpribeiro@gmail.com>
2015-08-20 15:22:41 +0000

    Added de locale and pt

---
+++ Resources/lang/de/core.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'modal' => [
+        'title' => 'Bestätigung',
+        'confirmation-message' => 'Wollen Sie wirklich diesen Datensatz wirklich löschen?',
+    ],
+    'table' => [
+        'created at' => 'Erstellt am',
+        'actions' => 'Aktionen',
+        'thumbnail' => 'Miniaturbild',
+    ],
+    'tab' => [
+        'english' => 'Englisch',
+        'french' => 'Französisch',
+        'dutch' =>  'Holländisch',
+        'italian' => 'Italienisch',
+        'greek' => 'Griechisch',
+        'spanish' => 'Spanisch',
+    ],
+    'button' => [
+        'cancel' => 'Abbrechen',
+        'create' => 'Erstellen',
+        'update' => 'Aktualisieren',
+        'delete' => 'Löschen',
+        'reset' => 'Zurücksetzen',
+        'update and back' => 'Aktualisieren und zuruck',
+    ],
+    'breadcrumb' => [
+        'home' => 'Startseite',
+    ],
+    'title' => [
+        'translatable fields' => 'Übersetzbaren Felder',
+        'non translatable fields' => 'Nicht übersetzbare Felder',
+        'create resource' => 'Erstellen :name',
+        'edit resource' => 'Bearbeiten :name',
+    ],
+    'general' => [
+        'available keyboard shortcuts' => 'Verfügbare Tastaturbefehle auf dieser Seite',
+        'view website' => 'Website anschauen',
+        'complete your profile' => 'Runden Ihr Profil',
+        'sign out' => 'Sign out',
+    ],
+    'messages' => [
+        'resource created' => ':name wurde erfolgreich erstellt.',
+        'resource not found' => ':name wurde nicht gefunden.',
+        'resource updated' => ':name wurde erfolgreich aktualisiert.',
+        'resource deleted' => ':name wurde erfolgreich gelöscht.',
+    ],
+    'back' => 'Zurück',
+    'back to index' => 'Zurück zu den :name Index',
+    'permission denied' => 'Zugriff verweigert. (erforderliche Berechtigung: ":permission")',
+];
