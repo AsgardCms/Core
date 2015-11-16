64e483dcf41b589cd099af58f9e24c43184d3446
Nicolas Widart <n.widart@gmail.com>
2015-11-16 14:16:16 +0100

    Adding a view composer to attacht he currently logged in user in the $currentUser variable

---
+++ Composers/CurrentUserViewComposer.php
@@ -0,0 +1,22 @@
+<?php namespace Modules\Core\Composers;
+
+use Illuminate\Contracts\View\View;
+use Modules\Core\Contracts\Authentication;
+
+class CurrentUserViewComposer
+{
+    /**
+     * @var Authentication
+     */
+    private $auth;
+
+    public function __construct(Authentication $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function compose(View $view)
+    {
+        $view->with('currentUser', $this->auth->check());
+    }
+}
