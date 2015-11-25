5c70fab891652cce0c67c62a09f8fc4432928094
Selahattin Ünlü <selahattin.unlu@yandex.com>
2015-11-25 17:27:05 +0200

    add turkish language support

---
+++ Resources/lang/tr/core.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+    'modal' => [
+        'title' => 'Onaylama',
+        'confirmation-message' => 'Bu kaydı gerçekten silmek istiyor musunuz?',
+    ],
+    'table' => [
+        'created at' => 'Oluşturulma Tarihi',
+        'actions' => 'İşlemler',
+        'thumbnail' => 'Thumbnail',
+    ],
+    'tab' => [
+        'english' => 'İngilizce',
+        'french' => 'Fransızca',
+        'dutch' => 'Flemenkçe',
+        'italian' => 'İtalyanca',
+        'greek' => 'Ελληνικά',
+        'spanish' => 'İspanyolca',
+        'polish' => 'Lehçe',
+        'turkish' => 'Türkçe',
+    ],
+    'button' => [
+        'cancel' => 'Vazgeç',
+        'create' => 'Oluştur',
+        'update' => 'Güncelle',
+        'delete' => 'Sil',
+        'reset' => 'Reset',
+        'update and back' => 'Güncelle ve geri git.',
+    ],
+    'breadcrumb' => [
+        'home' => 'Anasayfa',
+    ],
+    'title' => [
+        'translatable fields' => 'Çevirilebilir alanlar',
+        'non translatable fields' => 'Çevirilemez alanlar',
+        'create resource' => ':name oluştur',
+        'edit resource' => ':name düzenle',
+    ],
+    'general' => [
+        'available keyboard shortcuts' => 'Bu sayfada kullanılabilir kısayollar',
+        'view website' => 'Siteyi Görüntüle',
+        'complete your profile' => 'Profili tamamla',
+        'sign out' => 'Çıkış yap',
+    ],
+    'messages' => [
+        'resource created' => ':name başarıyla oluşturuldu.',
+        'resource not found' => ':name bulunamadı.',
+        'resource updated' => ':name başarıyla güncellendi.',
+        'resource deleted' => ':name başarıyla silindi.',
+    ],
+    'back' => 'Back',
+    'back to index' => ':name sayfasına geri dön.',
+    'permission denied' => 'Yetkiniz yok. (Gereken yetki: ":permission")',
+];
