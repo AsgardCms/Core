55b1b21790be64d17b93c481b50f36168830abd5
Nicolas Widart <n.widart@gmail.com>
2015-11-30 16:16:06 +0100

    Register the translationSP conditionally, thus removing the hard dependency

--- Providers/AsgardServiceProvider.php
+++ Providers/AsgardServiceProvider.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
+use Modules\Translation\Providers\TranslationServiceProvider;
 
 class AsgardServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        if (class_exists(TranslationServiceProvider::class)) {
+            $this->app->register(TranslationServiceProvider::class);
+        }
         $this->app->register('Pingpong\Modules\ModulesServiceProvider');
 
         $loader = AliasLoader::getInstance();
