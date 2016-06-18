c0a621a580e44bcca7a7c3c0cba0b0e536ad7ca9
Nicolas Widart <n.widart@gmail.com>
2016-06-18 16:11:59 +0200

    Adding 3 main asset types

---
+++ Foundation/Asset/Types/ModuleAsset.php
@@ -0,0 +1,30 @@
+<?php namespace Modules\Core\Foundation\Asset\Types;
+
+use Pingpong\Modules\Facades\Module;
+
+class ModuleAsset implements AssetType
+{
+    /**
+     * @var
+     */
+    private $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+    
+    /**
+     * Get the URL
+     * @return string
+     */
+    public function url()
+    {
+        return Module::asset($this->getPath());
+    }
+
+    private function getPath()
+    {
+        return array_get($this->path, 'module');
+    }
+}
