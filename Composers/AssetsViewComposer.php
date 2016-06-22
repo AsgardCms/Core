8d856f61bd2f4f3ceab388066d9d975067fe044c
Nicolas Widart <n.widart@gmail.com>
2016-06-22 17:53:27 +0200

    Only enable on backend

--- Composers/AssetsViewComposer.php
+++ Composers/AssetsViewComposer.php
@@ -1,6 +1,7 @@
 <?php namespace Modules\Core\Composers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 use Modules\Core\Foundation\Asset\Manager\AssetManager;
 use Modules\Core\Foundation\Asset\Pipeline\AssetPipeline;
 use Modules\Core\Foundation\Asset\Types\AssetTypeFactory;
@@ -19,16 +20,24 @@ class AssetsViewComposer
      * @var AssetTypeFactory
      */
     protected $assetFactory;
+    /**
+     * @var Request
+     */
+    private $request;
 
-    public function __construct(AssetManager $assetManager, AssetPipeline $assetPipeline, AssetTypeFactory $assetTypeFactory)
+    public function __construct(AssetManager $assetManager, AssetPipeline $assetPipeline, AssetTypeFactory $assetTypeFactory, Request $request)
     {
         $this->assetManager = $assetManager;
         $this->assetPipeline = $assetPipeline;
         $this->assetFactory = $assetTypeFactory;
+        $this->request = $request;
     }
 
     public function compose(View $view)
     {
+        if ($this->onBackend() === false) {
+            return;
+        }
         $this->requireDefaultAssets();
 
         $view->with('cssFiles', $this->assetPipeline->allCss());
@@ -42,5 +51,14 @@ class AssetsViewComposer
     {
         $this->assetPipeline->requireCss(config('asgard.core.core.admin-required-assets.css'));
         $this->assetPipeline->requireJs(config('asgard.core.core.admin-required-assets.js'));
+    }
+
+    private function onBackend()
+    {
+        $url = $this->request->url();
+        if (str_contains($url, config('asgard.core.core.admin-prefix'))) {
+            return true;
+        }
+        return false;
     }
 }
