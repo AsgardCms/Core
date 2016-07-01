42dd9948f771a9033d32af9d757bcc8c2edbe403
Nicolas Widart <n.widart@gmail.com>
2016-07-01 13:20:36 +0200

    Use the new configuration file path

--- Console/Installers/Scripts/UserProviders/SentinelInstaller.php
+++ Console/Installers/Scripts/UserProviders/SentinelInstaller.php
@@ -94,7 +94,7 @@ class SentinelInstaller extends ProviderInstaller implements SetupScript
      */
     private function changeDefaultUserProvider($driver)
     {
-        $path = base_path('config/asgard.user.users.php');
+        $path = base_path('config/asgard/user/users.php');
         $config = $this->finder->get($path);
         $config = str_replace('Sentry', $driver, $config);
         $this->finder->put($path, $config);
