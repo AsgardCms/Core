209748a8a55841ea5b2b9554faf7274aee9e7ce4
Nicolas Widart <n.widart@gmail.com>
2016-07-05 15:43:52 +0200

    Updating laravel-elixir mix template

--- stylistPublishMix.js
+++ stylistPublishMix.js
@@ -1,10 +1,12 @@
 var gulp = require("gulp");
 var shell = require('gulp-shell');
-var elixir = require("laravel-elixir");
+var elixir = require('laravel-elixir');
+var themeInfo = require('./theme.json');
 
-elixir.extend("stylistPublish", function() {
-    gulp.task("stylistPublish", function() {
-        gulp.src("").pipe(shell("php ../../artisan stylist:publish"));
+var Task = elixir.Task;
+
+elixir.extend('stylistPublish', function() {
+    new Task('stylistPublish', function() {
+        return gulp.src("").pipe(shell("php ../../artisan stylist:publish " + themeInfo.name));
     });
-    return this.queueTask("stylistPublish");
 });
