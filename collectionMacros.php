a362bfdf98fc740933f49fd4f437de2a43608a90
Nicolas Widart <n.widart@gmail.com>
2016-07-26 17:05:08 +0200

    Check if the macro exists

--- collectionMacros.php
+++ collectionMacros.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Support\Collection;
 
-// Source: https://adamwathan.me/2016/07/14/customizing-keys-when-mapping-collections/
-Collection::macro('toAssoc', function () {
-    return $this->reduce(function ($assoc, $keyValuePair) {
-        list($key, $value) = $keyValuePair;
-        $assoc[$key] = $value;
-        return $assoc;
-    }, new static);
-});
+if (Collection::hasMacro('toAssoc') === false) {
+    // Source: https://adamwathan.me/2016/07/14/customizing-keys-when-mapping-collections/
+    Collection::macro('toAssoc', function () {
+        return $this->reduce(function ($assoc, $keyValuePair) {
+            list($key, $value) = $keyValuePair;
+            $assoc[$key] = $value;
+            return $assoc;
+        }, new static);
+    });
+}
 
-Collection::macro('mapToAssoc', function ($callback) {
-    return $this->map($callback)->toAssoc();
-});
+if (Collection::hasMacro('mapToAssoc') === false) {
+    Collection::macro('mapToAssoc', function ($callback) {
+        return $this->map($callback)->toAssoc();
+    });
+}
