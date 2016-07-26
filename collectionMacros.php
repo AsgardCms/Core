efe6724e137114f8a49cff454e4f9eb09c28675d
Nicolas Widart <n.widart@gmail.com>
2016-07-26 14:16:14 +0200

    Adding source link

--- collectionMacros.php
+++ collectionMacros.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Collection;
 
+// Source: https://adamwathan.me/2016/07/14/customizing-keys-when-mapping-collections/
 Collection::macro('toAssoc', function () {
     return $this->reduce(function ($assoc, $keyValuePair) {
         list($key, $value) = $keyValuePair;
