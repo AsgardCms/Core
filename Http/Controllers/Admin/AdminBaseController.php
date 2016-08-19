52a6a0bcd5e97c985431c622ae82dbbce0cbf86e
Andrew Bekesh <arma@blackarma.com>
2016-08-19 23:24:03 +0300

    Partial revert of commit 4a1222
    Bring back assets to pipeline to be used by Controllers that extend AdminBaseController

--- Http/Controllers/Admin/AdminBaseController.php
+++ Http/Controllers/Admin/AdminBaseController.php
@@ -29,10 +29,11 @@ class AdminBaseController extends Controller
         $this->assetFactory = app(AssetTypeFactory::class);
 
         $this->addAssets();
+        $this->requireDefaultAssets();
     }
 
     /**
-     * Add the assets from the config file on the asset manager
+     * Add the assets from the config file on the asset manager.
      */
     private function addAssets()
     {
@@ -40,5 +41,14 @@ class AdminBaseController extends Controller
             $path = $this->assetFactory->make($path)->url();
             $this->assetManager->addAsset($assetName, $path);
         }
+    }
+
+    /**
+     * Require the default assets from config file on the asset pipeline.
+     */
+    private function requireDefaultAssets()
+    {
+        $this->assetPipeline->requireCss(config('asgard.core.core.admin-required-assets.css'));
+        $this->assetPipeline->requireJs(config('asgard.core.core.admin-required-assets.js'));
     }
 }
